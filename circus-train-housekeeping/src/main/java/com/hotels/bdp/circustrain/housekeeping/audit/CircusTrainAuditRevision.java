9437c104574b7f90a6b004c534f0389d6b06ae38
Courtney Edwards <cedwards@hotels.com>
2018-01-02 11:34:00 +0000

    Use entity inheritance for tables

--- circus-train-housekeeping/src/main/java/com/hotels/bdp/circustrain/housekeeping/audit/CircusTrainAuditRevision.java
+++ circus-train-housekeeping/src/main/java/com/hotels/bdp/circustrain/housekeeping/audit/CircusTrainAuditRevision.java
@@ -15,82 +15,15 @@
  */
 package com.hotels.bdp.circustrain.housekeeping.audit;
 
-import java.util.Date;
-
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 import org.hibernate.envers.RevisionEntity;
-import org.hibernate.envers.RevisionNumber;
-import org.hibernate.envers.RevisionTimestamp;
 
-import com.hotels.housekeeping.audit.AuditRevision;
+import com.hotels.housekeeping.audit.EntityAuditRevision;
 
 @Entity
 @RevisionEntity
 @Table(schema = "circus_train", name = "audit_revision")
-public class CircusTrainAuditRevision implements AuditRevision {
-
-  private static final long serialVersionUID = 1L;
-
-  @Id
-  @GeneratedValue
-  @RevisionNumber
-  @Column(name = "id")
-  private int id;
-
-  @RevisionTimestamp
-  @Column(name = "timestamp")
-  private long timestamp;
-
-  public int getId() {
-    return id;
-  }
-
-  public void setId(int id) {
-    this.id = id;
-  }
-
-  @Transient
-  public Date getRevisionDate() {
-    return new Date(timestamp);
-  }
-
-  public long getTimestamp() {
-    return timestamp;
-  }
-
-  public void setTimestamp(long timestamp) {
-    this.timestamp = timestamp;
-  }
-
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + id;
-    return result;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj) {
-      return true;
-    }
-    if (obj == null) {
-      return false;
-    }
-    if (getClass() != obj.getClass()) {
-      return false;
-    }
-    CircusTrainAuditRevision other = (CircusTrainAuditRevision) obj;
-    if (id != other.id) {
-      return false;
-    }
-    return true;
-  }
+public class CircusTrainAuditRevision extends EntityAuditRevision {
 }
--- circus-train-housekeeping/src/main/java/com/hotels/bdp/circustrain/housekeeping/model/CircusTrainLegacyReplicaPath.java
+++ circus-train-housekeeping/src/main/java/com/hotels/bdp/circustrain/housekeeping/model/CircusTrainLegacyReplicaPath.java
@@ -15,109 +15,26 @@
  */
 package com.hotels.bdp.circustrain.housekeeping.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
 import org.hibernate.envers.Audited;
-import org.joda.time.Instant;
 
-import com.google.common.base.Objects;
-
-import com.hotels.housekeeping.model.LegacyReplicaPath;
+import com.hotels.housekeeping.model.EntityLegacyReplicaPath;
 
 @Entity
 // @Converter doesn't work with @Audited until https://hibernate.atlassian.net/browse/HHH-9042 is released
 @Audited
 @Table(schema = "circus_train", name = "legacy_replica_path",
     uniqueConstraints = @UniqueConstraint(columnNames = { "path", "creation_timestamp" }))
-public class CircusTrainLegacyReplicaPath implements LegacyReplicaPath {
-
-  private static final long serialVersionUID = 1L;
-
-  @Id
-  @GeneratedValue
-  @Column(name = "id")
-  protected long id;
-
-  @Column(name = "event_id", nullable = false, length = 250)
-  protected String eventId;
-
-  @Column(name = "path", nullable = false, length = 10000)
-  protected String path;
-
-  @Column(name = "creation_timestamp", nullable = false, updatable = false)
-  protected long creationTimestamp;
-
-  @Column(name = "path_event_id", nullable = true, length = 250)
-  protected String pathEventId;
+public class CircusTrainLegacyReplicaPath extends EntityLegacyReplicaPath {
 
   protected CircusTrainLegacyReplicaPath() {}
-
-  public CircusTrainLegacyReplicaPath(String path) {
-    this.path = path;
-    this.eventId = "";
-    this.pathEventId = "";
-  }
 
   public CircusTrainLegacyReplicaPath(String eventId, String pathEventId, String path) {
     this.eventId = eventId;
     this.pathEventId = pathEventId;
     this.path = path;
-  }
-
-  public long getId() {
-    return id;
-  }
-
-  public String getEventId() {
-    return eventId;
-  }
-
-  public String getPathEventId() {
-    return pathEventId;
-  }
-
-  public String getPath() {
-    return path;
-  }
-
-  public long getCreationTimestamp() {
-    return creationTimestamp;
-  }
-
-  public void setPath(String path) { this.path = path;}
-
-  public void setPathEventId(String pathEventId) {
-    this.pathEventId = pathEventId;
-  }
-
-  public void setEventId(String eventId) {
-    this.eventId = eventId;
-  }
-
-  public void setCreationTimestamp(long creationTimestamp) {
-    this.creationTimestamp = creationTimestamp;
-  }
-
-  @PrePersist
-  protected void onPersist() {
-    setCreationTimestamp(new Instant().getMillis());
-  }
-
-  @Override
-  public String toString() {
-    return Objects
-        .toStringHelper(this)
-        .add("id", id)
-        .add("eventId", eventId)
-        .add("pathEventId", pathEventId)
-        .add("path", path)
-        .add("creationTimestamp", creationTimestamp)
-        .toString();
   }
 }
