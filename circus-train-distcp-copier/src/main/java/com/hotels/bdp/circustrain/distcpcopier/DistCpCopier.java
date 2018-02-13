28e917e3d5f62527331a512399f2ddc9ce2c56ba
Courtney Edwards <cedwards@hotels.com>
2018-02-13 16:09:37 +0000

    Import order

--- circus-train-distcp-copier/src/main/java/com/hotels/bdp/circustrain/distcpcopier/DistCpCopier.java
+++ circus-train-distcp-copier/src/main/java/com/hotels/bdp/circustrain/distcpcopier/DistCpCopier.java
@@ -15,12 +15,13 @@
  */
 package com.hotels.bdp.circustrain.distcpcopier;
 
-import static java.util.Collections.singletonList;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-
+import com.codahale.metrics.Gauge;
+import com.codahale.metrics.MetricRegistry;
+import com.hotels.bdp.circustrain.api.CircusTrainException;
+import com.hotels.bdp.circustrain.api.copier.Copier;
+import com.hotels.bdp.circustrain.api.metrics.Metrics;
+import com.hotels.bdp.circustrain.core.util.LibJarDeployer;
+import com.hotels.bdp.circustrain.metrics.JobMetrics;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
@@ -31,24 +32,35 @@ import org.apache.hadoop.tools.DistCpOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.codahale.metrics.Gauge;
-import com.codahale.metrics.MetricRegistry;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
-import com.hotels.bdp.circustrain.api.CircusTrainException;
-import com.hotels.bdp.circustrain.api.copier.Copier;
-import com.hotels.bdp.circustrain.api.metrics.Metrics;
-import com.hotels.bdp.circustrain.core.util.LibJarDeployer;
-import com.hotels.bdp.circustrain.metrics.JobMetrics;
+import static java.util.Collections.singletonList;
 
 public class DistCpCopier implements Copier {
 
+  interface DistCpExecutor {
+
+    static final DistCpExecutor DEFAULT = new DistCpExecutor() {
+      @Override
+      public Job exec(Configuration conf, DistCpOptions options) throws Exception {
+        return new DistCp(conf, options).execute();
+      }
+    };
+
+    Job exec(Configuration conf, DistCpOptions options) throws Exception;
+  }
+
   private static final Logger LOG = LoggerFactory.getLogger(DistCpCopier.class);
+
   private final Configuration conf;
   private final Path sourceDataBaseLocation;
   private final List<Path> sourceDataLocations;
   private final Path replicaDataLocation;
   private final Map<String, Object> copierOptions;
   private final DistCpExecutor executor;
+
   private final MetricRegistry registry;
 
   public DistCpCopier(
@@ -149,17 +161,5 @@ public class DistCpCopier implements Copier {
     } catch (Exception e) {
       LOG.error("Unable to clean up replica data location {} after DistCp failure", replicaDataLocation.toUri(), e);
     }
-  }
-
-  interface DistCpExecutor {
-
-    static final DistCpExecutor DEFAULT = new DistCpExecutor() {
-      @Override
-      public Job exec(Configuration conf, DistCpOptions options) throws Exception {
-        return new DistCp(conf, options).execute();
-      }
-    };
-
-    Job exec(Configuration conf, DistCpOptions options) throws Exception;
   }
 }
