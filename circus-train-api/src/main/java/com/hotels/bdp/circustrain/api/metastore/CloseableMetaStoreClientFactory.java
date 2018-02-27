50b3157b9cf59f1bae2cd9d3680c5813cd764734
Daniel del Castillo <ddcprg@users.noreply.github.com>
2018-02-27 12:23:09 +0000

    [issue-32] Add compatibility with Hive 1.2.x (#38)

--- circus-train-api/src/main/java/com/hotels/bdp/circustrain/api/metastore/CloseableMetaStoreClientFactory.java
+++ circus-train-api/src/main/java/com/hotels/bdp/circustrain/api/metastore/CloseableMetaStoreClientFactory.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2016-2017 Expedia Inc.
+ * Copyright (C) 2016-2018 Expedia Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,24 +21,51 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 import org.apache.hadoop.hive.metastore.IMetaStoreClient;
+import org.apache.thrift.TApplicationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.annotations.VisibleForTesting;
+
+import com.hotels.bdp.circustrain.api.metastore.compatibility.HiveMetaStoreClientCompatibility;
+import com.hotels.bdp.circustrain.api.metastore.compatibility.HiveMetaStoreClientCompatibility12x;
 
 public final class CloseableMetaStoreClientFactory {
+  private static final Logger log = LoggerFactory.getLogger(CloseableMetaStoreClientFactory.class);
 
   private CloseableMetaStoreClientFactory() {}
 
   public static CloseableMetaStoreClient newInstance(IMetaStoreClient delegate) {
+    HiveMetaStoreClientCompatibility compatibility = null;
+    try {
+      compatibility = new HiveMetaStoreClientCompatibility12x(delegate);
+    } catch (Throwable t) {
+      log.warn("Unable to initialize compatibility", t);
+    }
+    return newInstance(delegate, compatibility);
+  }
+
+  @VisibleForTesting
+  static CloseableMetaStoreClient newInstance(
+      IMetaStoreClient delegate,
+      HiveMetaStoreClientCompatibility compatibility) {
     ClassLoader classLoader = CloseableMetaStoreClient.class.getClassLoader();
     Class<?>[] interfaces = new Class<?>[] { CloseableMetaStoreClient.class };
-    CloseableMetaStoreClientInvocationHandler handler = new CloseableMetaStoreClientInvocationHandler(delegate);
+    CloseableMetaStoreClientInvocationHandler handler = new CloseableMetaStoreClientInvocationHandler(delegate,
+        compatibility);
     return (CloseableMetaStoreClient) Proxy.newProxyInstance(classLoader, interfaces, handler);
   }
 
   static class CloseableMetaStoreClientInvocationHandler implements InvocationHandler {
 
     private final IMetaStoreClient delegate;
+    private final HiveMetaStoreClientCompatibility compatibility;
 
-    CloseableMetaStoreClientInvocationHandler(IMetaStoreClient delegate) {
+    CloseableMetaStoreClientInvocationHandler(
+        IMetaStoreClient delegate,
+        HiveMetaStoreClientCompatibility compatibility) {
       this.delegate = delegate;
+      this.compatibility = compatibility;
     }
 
     @Override
@@ -46,8 +73,30 @@ public final class CloseableMetaStoreClientFactory {
       try {
         return method.invoke(delegate, args);
       } catch (InvocationTargetException e) {
+        try {
+          if (compatibility != null && e.getCause().getClass().isAssignableFrom(TApplicationException.class)) {
+            return invokeCompatibility(method, args);
+          }
+        } catch (Throwable t) {
+          log.warn("Unable to run compatibility for metastore client method {}. Will rethrow original exception: ",
+              method.getName(), t);
+        }
         throw e.getCause();
       }
+    }
+
+    private Object invokeCompatibility(Method method, Object[] args) throws Throwable {
+      Class<?>[] argTypes = getTypes(args);
+      Method compatibilityMethod = compatibility.getClass().getMethod(method.getName(), argTypes);
+      return compatibilityMethod.invoke(compatibility, args);
+    }
+
+    private static Class<?>[] getTypes(Object[] args) {
+      Class<?>[] argTypes = new Class<?>[args.length];
+      for (int i = 0; i < args.length; ++i) {
+        argTypes[i] = args[i].getClass();
+      }
+      return argTypes;
     }
 
   }
