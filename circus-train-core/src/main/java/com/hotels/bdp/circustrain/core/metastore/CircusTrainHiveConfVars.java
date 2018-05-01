10c35e816e6d62e96315f2921682677d3f2e7e1f
Daniel del Castillo <ddcprg@users.noreply.github.com>
2018-05-01 12:17:40 +0100

    [issue-64] Add SSH timeout and SSH strick host key checking properties to metastore tunnel (#65)

--- circus-train-core/src/main/java/com/hotels/bdp/circustrain/core/metastore/CircusTrainHiveConfVars.java
+++ circus-train-core/src/main/java/com/hotels/bdp/circustrain/core/metastore/CircusTrainHiveConfVars.java
@@ -15,20 +15,30 @@
  */
 package com.hotels.bdp.circustrain.core.metastore;
 
+import com.hotels.bdp.circustrain.core.conf.MetastoreTunnel;
+
 public enum CircusTrainHiveConfVars {
 
-  SSH_LOCALHOST("com.hotels.bdp.circustrain.core.metastore.client.ssh.localhost"),
-  SSH_PORT("com.hotels.bdp.circustrain.core.metastore.client.ssh.port"),
-  SSH_ROUTE("com.hotels.bdp.circustrain.core.metastore.client.ssh.route"),
-  SSH_PRIVATE_KEYS("com.hotels.bdp.circustrain.core.metastore.client.ssh.private_keys"),
-  SSH_KNOWN_HOSTS("com.hotels.bdp.circustrain.core.metastore.client.ssh.known_hosts"),
-  SSH_SESSION_TIMEOUT("com.hotels.bdp.circustrain.core.metastore.client.ssh.session_timeout"),
-  SSH_STRICT_HOST_KEY_CHECKING("com.hotels.bdp.circustrain.core.metastore.client.ssh.strict_host_key_checking");
+  SSH_LOCALHOST("com.hotels.bdp.circustrain.core.metastore.client.ssh.localhost", MetastoreTunnel.DEFAULT_LOCALHOST),
+  SSH_PORT("com.hotels.bdp.circustrain.core.metastore.client.ssh.port", MetastoreTunnel.DEFAULT_PORT),
+  SSH_ROUTE("com.hotels.bdp.circustrain.core.metastore.client.ssh.route", null),
+  SSH_PRIVATE_KEYS("com.hotels.bdp.circustrain.core.metastore.client.ssh.private_keys", null),
+  SSH_KNOWN_HOSTS("com.hotels.bdp.circustrain.core.metastore.client.ssh.known_hosts", null),
+  SSH_SESSION_TIMEOUT("com.hotels.bdp.circustrain.core.metastore.client.ssh.session_timeout",
+      MetastoreTunnel.DEFAULT_SESSION_TIMEOUT),
+  SSH_STRICT_HOST_KEY_CHECKING("com.hotels.bdp.circustrain.core.metastore.client.ssh.strict_host_key_checking",
+      MetastoreTunnel.DEFAULT_STRICT_HOST_KEY_CHECK);
 
   public final String varname;
+  public final String defaultValue;
 
-  private CircusTrainHiveConfVars(String varname) {
+  private CircusTrainHiveConfVars(String varname, int defaultValue) {
+    this(varname, String.valueOf(defaultValue));
+  }
+
+  private CircusTrainHiveConfVars(String varname, String defaultValue) {
     this.varname = varname;
+    this.defaultValue = defaultValue;
   }
 
 }
