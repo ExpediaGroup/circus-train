8903783beb57bf05d86dd82f60f6091cd3e46551
Courtney Edwards <cedwards@hotels.com>
2018-05-18 14:08:57 +0100

    More generic paths

--- circus-train-avro/src/test/java/com/hotels/bdp/circustrain/avro/util/NameServicePathResolverTest.java
+++ circus-train-avro/src/test/java/com/hotels/bdp/circustrain/avro/util/NameServicePathResolverTest.java
@@ -30,60 +30,60 @@ public class NameServicePathResolverTest {
   @Test
   public void resolveNameServiceAddsAuthorityFromRootPath() {
     setDfsNameservices("hdp-ha");
-    assertEquals("hdfs://hdp-ha/etl/test/avsc0/tlog.avsc",
-        new NameServicePathResolver(configuration).resolve("hdfs:/etl/test/avsc0/tlog.avsc").toString());
+    assertEquals("hdfs://hdp-ha/etl/test/avsc/schema.avsc",
+        new NameServicePathResolver(configuration).resolve("hdfs:/etl/test/avsc/schema.avsc").toString());
   }
 
   @Test
   public void resolveNameServiceAddsAuthorityFromFullyQualifiedRootPath() {
     setDfsNameservices("hdp-ha");
-    assertEquals("hdfs://hdp-ha/etl/test/avsc0/tlog.avsc",
-        new NameServicePathResolver(configuration).resolve("hdfs:///etl/test/avsc0/tlog.avsc").toString());
+    assertEquals("hdfs://hdp-ha/etl/test/avsc/schema.avsc",
+        new NameServicePathResolver(configuration).resolve("hdfs:///etl/test/avsc/schema.avsc").toString());
   }
 
   @Test
   public void resolveNameServiceAddsAuthorityFromPathWithoutScheme() {
     setDfsNameservices("hdp-ha");
-    assertEquals("/hdp-ha/etl/test/avsc0/tlog.avsc",
-        new NameServicePathResolver(configuration).resolve("/etl/test/avsc0/tlog.avsc").toString());
+    assertEquals("/hdp-ha/etl/test/avsc/schema.avsc",
+        new NameServicePathResolver(configuration).resolve("/etl/test/avsc/schema.avsc").toString());
   }
 
   @Test
   public void resolveEmptyNameServiceReturnsOriginalPathFromFullyQualifiedRootPath() {
     setDfsNameservices("");
-    Path path = new Path("hdfs:///etl/test/avsc0/tlog.avsc");
+    Path path = new Path("hdfs:///etl/test/avsc/schema.avsc");
     assertEquals(path.toString(), new NameServicePathResolver(configuration).resolve(path.toString()).toString());
   }
 
   @Test
   public void resolveEmptyNameServiceReturnsOriginalPathFromRootPath() {
     setDfsNameservices("");
-    Path path = new Path("hdfs:/etl/test/avsc0/tlog.avsc");
+    Path path = new Path("hdfs:/etl/test/avsc/schema.avsc");
     assertEquals(path.toString(), new NameServicePathResolver(configuration).resolve(path.toString()).toString());
   }
 
   @Test
   public void resolveEmptyNameServiceReturnsOriginalPathFromPathWithoutScheme() {
     setDfsNameservices("");
-    Path path = new Path("/etl/test/avsc0/tlog.avsc");
+    Path path = new Path("/etl/test/avsc/schema.avsc");
     assertEquals(path.toString(), new NameServicePathResolver(configuration).resolve(path.toString()).toString());
   }
 
   @Test
   public void resolveNullNameServiceReturnsOriginalPathFromFullyQualifiedRootPath() {
-    Path path = new Path("hdfs:///etl/test/avsc0/tlog.avsc");
+    Path path = new Path("hdfs:///etl/test/avsc/schema.avsc");
     assertEquals(path.toString(), new NameServicePathResolver(configuration).resolve(path.toString()).toString());
   }
 
   @Test
   public void resolveNullNameServiceReturnsOriginalPathFromRootPath() {
-    Path path = new Path("hdfs:/etl/test/avsc0/tlog.avsc");
+    Path path = new Path("hdfs:/etl/test/avsc/schema.avsc");
     assertEquals(path.toString(), new NameServicePathResolver(configuration).resolve(path.toString()).toString());
   }
 
   @Test
   public void resolveNullNameServiceReturnsOriginalPathFromPathWithoutScheme() {
-    Path path = new Path("/etl/test/avsc0/tlog.avsc");
+    Path path = new Path("/etl/test/avsc/schema.avsc");
     assertEquals(path.toString(), new NameServicePathResolver(configuration).resolve(path.toString()).toString());
   }
 
