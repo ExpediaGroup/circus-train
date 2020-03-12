a4abfa2fd36a15b8493c230a5686d834327f409d
Ken Figueiredo <kfigueiredo@expediagroup.com>
2020-03-12 18:39:56 -0400

    add transfermanagerFactoryTest

---
+++ circus-train-s3-s3-copier/src/test/java/com/hotels/bdp/circustrain/s3s3copier/aws/TransferManagerFactoryTest.java
@@ -0,0 +1,41 @@
+package com.hotels.bdp.circustrain.s3s3copier.aws;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+import java.util.HashMap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.TransferManagerConfiguration;
+
+import com.hotels.bdp.circustrain.s3s3copier.S3S3CopierOptions;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TransferManagerFactoryTest {
+
+  @Mock private AmazonS3 mockClient;
+  private final Long MULTIPART_COPY_THRESHOLD_VALUE = 1L;
+  private final Long MULTIPART_COPY_PART_SIZE = 1L;
+
+  @Test
+  public void shouldCreateDefaultTransferManagerClient() {
+    S3S3CopierOptions s3Options = new S3S3CopierOptions(new HashMap<String, Object>() {{
+      put(S3S3CopierOptions.Keys.MULTIPART_COPY_THRESHOLD.keyName(), MULTIPART_COPY_THRESHOLD_VALUE);
+      put(S3S3CopierOptions.Keys.MULTIPART_COPY_PART_SIZE.keyName(), MULTIPART_COPY_PART_SIZE);
+    }});
+
+    TransferManagerFactory factory = new TransferManagerFactory();
+    TransferManager transferManager = factory.newInstance(mockClient, s3Options);
+    assertThat(transferManager.getAmazonS3Client(), is(mockClient));
+
+    TransferManagerConfiguration tmConfig = transferManager.getConfiguration();
+    assertThat(tmConfig.getMultipartCopyPartSize(), is(MULTIPART_COPY_PART_SIZE));
+    assertThat(tmConfig.getMultipartCopyThreshold(), is(MULTIPART_COPY_THRESHOLD_VALUE));
+  }
+}
